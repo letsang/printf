997b32411391bfec162a38101d04faeca423ad89
letsang <56046208+letsang@users.noreply.github.com>
2019-12-15 16:17:18 +0100

    Update ft_printf.c

--- ft_printf.c
+++ ft_printf.c
@@ -16,19 +16,39 @@
 
 int		ft_printf(const char *fmt, ...)
 {
-	va_list	ap;
-	int		d;
-	char	c, *s;
+	va_list			ap;
+	char			c, *s;
+	int				*p, d, i, x;
+	unsigned int	u;
 
 	va_start(ap, fmt);
 	while (*fmt)
 	{
-		if (*fmt == '%' && *(fmt + 1) == 's')
+		if (*fmt == '%' && *(fmt + 1) == 'c')
+		{
+			c = (int)va_arg(ap, int);
+			write(1, &c, 1);
+		}
+		else if (*fmt == '%' && *(fmt + 1) == 's')
+		{
 			s = va_arg(ap, char *);
-		else if (*fmt == '%' && *(fmt + 1) == 'c')
-			c = va_arg(ap, int);
+			ft_putstr(s);
+		}
+		else if (*fmt == '%' && *(fmt + 1) == 'p')
+			p = va_arg(ap, int	*);
 		else if (*fmt == '%' && *(fmt + 1) == 'd')
+		{
 			d = va_arg(ap, int);
+			ft_putnbr(d);
+		}
+		else if (*fmt == '%' && *(fmt + 1) == 'i')
+			i = va_arg(ap, int);
+		else if (*fmt == '%' && *(fmt + 1) == 'u')
+			u = va_arg(ap, unsigned int);
+		else if (*fmt == '%' && (*(fmt + 1) == 'x' || *(fmt + 1) == 'X'))
+			x = va_arg(ap, int);
+		else if (*fmt == '%' && *(fmt + 1) == '%')
+			write(1, "%", 1);
 		fmt++;
 	}
 	va_end(ap);
