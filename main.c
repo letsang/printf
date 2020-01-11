cf001fe3c735195ef3113b0d4d93e8539630c2e0
letsang <tsang.jacques@gmail.com>
2020-01-11 10:32:39 +0100

    ok main.c

--- main.c
+++ main.c
@@ -20,9 +20,24 @@ int		main(int ac, char **av)
 
 	n = -94400;
 	ret = 0;
+	ret = ft_printf("MY PRINTF : %s %015d, %p %X %% %d", av[1], n, &ac, n, n);
+	printf("\nRETURN VALUE : %d\n", ret); 
+	ret = printf("CP PRINTF : %s %015d, %p %X %% %d", av[1], n, &ac, n, n);
+	printf("\nRETURN VALUE : %d\n\n", ret);
+
+	ret = ft_printf("MY PRINTF : %s %-015d, %p %X %% %d", av[1], n, &ac, n, n);
+	printf("\nRETURN VALUE : %d\n", ret); 
+	ret = printf("CP PRINTF : %s %-015d, %p %X %% %d", av[1], n, &ac, n, n);
+	printf("\nRETURN VALUE : %d\n\n", ret);
+
+	ret = ft_printf("MY PRINTF : %s %-15d, %p %X %% %d", av[1], n, &ac, n, n);
+	printf("\nRETURN VALUE : %d\n", ret); 
+	ret = printf("CP PRINTF : %s %-15d, %p %X %% %d", av[1], n, &ac, n, n);
+	printf("\nRETURN VALUE : %d\n\n", ret);
+
 	ret = ft_printf("MY PRINTF : %s %15d, %p %X %% %d", av[1], n, &ac, n, n);
 	printf("\nRETURN VALUE : %d\n", ret); 
 	ret = printf("CP PRINTF : %s %15d, %p %X %% %d", av[1], n, &ac, n, n);
-	printf("\nRETURN VALUE : %d", ret);
+	printf("\nRETURN VALUE : %d\n\n", ret);
 	return (0); 
 }
