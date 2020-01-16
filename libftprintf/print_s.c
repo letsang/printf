35f820ced93530001d570a1cb33e64f1808a15d9
letsang <tsang.jacques@gmail.com>
2020-01-16 08:47:44 +0100

    ok print_s wip

--- libftprintf/print_s.c
+++ libftprintf/print_s.c
@@ -12,7 +12,17 @@
 
 #include "ft_printf.h"
 
-int		ft_putstr(const char *s)
+int		ft_strlen(const char *s)
+{
+	int				i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+int		ft_putstr(const char *s, t_flag list_flag)
 {
 	int				ret;
 	int				i;
@@ -23,20 +33,83 @@ int		ft_putstr(const char *s)
 	if (s)
 	{
 		i = 0;
-		while (s[i])
+		if (list_flag.precision < ft_strlen(s))
 		{
-			ret += write(1, &s[i], 1);
-			i++;
+			while (s[i] && (i < list_flag.precision))
+			{
+				ret += write(1, &s[i], 1);
+				i++;
+			}
 		}
+		else
+		{
+			while (s[i])
+			{
+				ret += write(1, &s[i], 1);
+				i++;
+			}
+		}
+	}
+	return (ret);
+}
+
+int		print_s_space(int *width)
+{
+	int		ret;
+
+	ret = 0;
+	while (*width > 0)
+	{
+		ret += ft_putchar(' ');
+		(*width)--;
+	}
+	return (ret);
+}
+
+int		print_s_padding(t_flag *list_flag, int *width)
+{
+	int		ret;
+
+	ret = 0;
+	while (*width > 0)
+	{
+		if (list_flag->padding && *width && !list_flag->precision)
+		{
+			ret += ft_putchar('0');
+			(*width)--;
+		}
+		else
+			ret += print_s_space(width);
 	}
 	return (ret);
 }
 
 int		print_s(va_list av, t_flag list_flag)
 {
+	int			ret;
+	int			width;
+	int			precision;
 	char			*s;
 
-	(void)list_flag;
+	ret = 0;
 	s = va_arg(av, char *);
-	return (ft_putstr(s));
+	precision = (list_flag.precision) ?
+			list_flag.precision - ft_strlen(s) : 0;
+	width = (precision > 0) ? list_flag.width - (ft_strlen(s) + precision) : list_flag.width - ft_strlen(s);
+	if (width > 0)
+	{
+		if (list_flag.justify)
+		{
+			ret += ft_putstr(s, list_flag);
+			ret += print_s_space(&width);
+		}
+		else
+		{
+			ret += print_s_padding(&list_flag, &width);
+			ret += ft_putstr(s, list_flag);
+		}
+	}	
+	else
+		ret += ft_putstr(s, list_flag);
+	return (ret);
 }
