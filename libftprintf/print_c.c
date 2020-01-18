a36a88abf62329167428b8afd9a62f25f1151da6
letsang <tsang.jacques@gmail.com>
2020-01-18 13:47:59 +0100

    ok 722/745 passed

--- libftprintf/print_c.c
+++ libftprintf/print_c.c
@@ -12,11 +12,39 @@
 
 #include "ft_printf.h"
 
+int		print_c_space(int *width)
+{
+	int		ret;
+
+	ret = 0;
+	while (*width > 0)
+	{
+		ret += ft_putchar(' ');
+		(*width)--;
+	}
+	return (ret);
+}
+
 int		print_c(va_list av, t_flag list_flag)
 {
+	int			ret;
+	int			width;
 	char			c;
 
-	(void)list_flag;
+	ret = 0;
 	c = va_arg(av, int);
-	return (ft_putchar(c));
+	width = list_flag.width - 1;
+	if (width > 0)
+	{
+		if (list_flag.justify)
+		{
+			ret += ft_putchar(c);
+			ret += print_c_space(&width);
+		}
+		else
+			ret += ft_putchar(c);
+	}	
+	else
+		ret += ft_putchar(c);
+	return (ret);
 }
